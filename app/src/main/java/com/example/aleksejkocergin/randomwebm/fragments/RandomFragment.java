0ce9d6fbc526548fe5c016a6298ea26eac9fbc9e
Alexey Kochergin <alkocherg@gmail.com>
2018-02-10 21:32:04 +0700

    Exoplayer using dependency injection.

--- app/src/main/java/com/example/aleksejkocergin/randomwebm/fragments/RandomFragment.java
+++ app/src/main/java/com/example/aleksejkocergin/randomwebm/fragments/RandomFragment.java
@@ -1,23 +1,16 @@
 package com.example.aleksejkocergin.randomwebm.fragments;
 
-import android.content.res.Configuration;
-import android.support.v4.app.Fragment;
-import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.FrameLayout;
-import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.apollographql.apollo.ApolloCall;
 import com.apollographql.apollo.api.Response;
@@ -26,33 +19,23 @@ import com.example.aleksejkocergin.myapplication.WebmQuery;
 import com.example.aleksejkocergin.randomwebm.R;
 import com.example.aleksejkocergin.randomwebm.RandomWebmApplication;
 import com.example.aleksejkocergin.randomwebm.adapter.TagsAdapter;
-import com.google.android.exoplayer2.C;
-import com.google.android.exoplayer2.ExoPlaybackException;
-import com.google.android.exoplayer2.ExoPlayer;
-import com.google.android.exoplayer2.ExoPlayerFactory;
-import com.google.android.exoplayer2.PlaybackParameters;
+import com.example.aleksejkocergin.randomwebm.interfaces.DaggerRandFragmentComponent;
+import com.example.aleksejkocergin.randomwebm.interfaces.RandFragmentComponent;
+import com.example.aleksejkocergin.randomwebm.module.ExoPlayerModule;
+import com.google.android.exoplayer2.LoadControl;
 import com.google.android.exoplayer2.SimpleExoPlayer;
-import com.google.android.exoplayer2.Timeline;
-import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
-import com.google.android.exoplayer2.extractor.ExtractorsFactory;
-import com.google.android.exoplayer2.source.ExtractorMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
-import com.google.android.exoplayer2.source.TrackGroupArray;
-import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
-import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.trackselection.TrackSelection;
-import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
 import com.google.android.exoplayer2.trackselection.TrackSelector;
-import com.google.android.exoplayer2.ui.PlaybackControlView;
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
-import com.google.android.exoplayer2.upstream.BandwidthMeter;
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
-import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
-import com.google.android.exoplayer2.util.Util;
+import com.google.android.exoplayer2.upstream.HttpDataSource;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -61,189 +44,105 @@ import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.observers.DisposableObserver;
 import io.reactivex.schedulers.Schedulers;
 
-public class RandomFragment extends Fragment implements View.OnClickListener, ExoPlayer.EventListener,
-        PlaybackControlView.VisibilityListener {
-
+public class RandomFragment extends Fragment {
     private static final String TAG = RandomFragment.class.getSimpleName();
-    private RandomWebmApplication application;
-    private CompositeDisposable disposables = new CompositeDisposable();
+    private String id = "";
+    private String tagName;
+    private String order = "createdAt";
+    private String videoUrl = "https://randomwebm.s3.eu-central-1.amazonaws.com/webms/68f8f8e2fc61f12a3ba69b6cb1323eb9.webm";
 
-    private DefaultBandwidthMeter defaultBandwidthMeter;
-    private DataSource.Factory mediaDataSourceFactory;
-    private SimpleExoPlayer player;
-    private ExtractorsFactory extractorsFactory;
-    private TagsAdapter tagsAdapter;
-    private LinearLayoutManager mLayoutManager;
-
-    @BindView(R.id.loading_bar)
-    ProgressBar progressBar;
+    @BindView(R.id.player_view)
+    SimpleExoPlayerView mPlayerView;
     @BindView(R.id.txt_createdAt)
     TextView createdAt;
     @BindView(R.id.txt_views)
-    TextView viewers;
+    TextView views;
     @BindView(R.id.next_button)
-    Button retryButton;
-    @BindView(R.id.player_view)
-    SimpleExoPlayerView simpleExoPlayerView;
-    @BindView(R.id.root)
-    View rootView;
-    @BindView(R.id.frame_player)
-    FrameLayout frameLayout;
+    Button randomButton;
     @BindView(R.id.tags_recycler_view)
-    RecyclerView tagsRecyclerView;
+    RecyclerView mTagsRecycler;
 
-    String videoUrl;
-    String id = "";
-    String order = "createdAt";
-    String tagName;
+    private LinearLayoutManager mLayoutManager;
+    private TagsAdapter mTagsAdapter;
+    private RandomWebmApplication mApplication;
+    private CompositeDisposable mDisposable = new CompositeDisposable();
 
-    private boolean shouldAutoPlay;
-    private int resumeWindow;
-    private long resumePosition;
+    @Inject
+    Handler handler;
+    @Inject
+    DefaultBandwidthMeter bandwidthMeter;
+    @Inject
+    TrackSelection.Factory videoTrackSelectionFactory;
+    @Inject
+    TrackSelector trackSelector;
+    @Inject
+    LoadControl loadControl;
+    @Inject
+    SimpleExoPlayer player;
+    @Inject
+    String userAgent;
+    @Inject
+    HttpDataSource.Factory httpDataSourceFactory;
+    @Inject
+    DataSource.Factory dataSourceFactory;
+    @Inject
+    MediaSource videoSource;
 
     public static RandomFragment newInstance() {
-        RandomFragment randomFragment = new RandomFragment();
-        return randomFragment;
+        return new RandomFragment();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.random_fragment, container, false);
         ButterKnife.bind(this, v);
-        application = (RandomWebmApplication) getActivity().getApplication();
-        retryButton.setOnClickListener(this);
-        rootView.setOnClickListener(this);
-        shouldAutoPlay = true;
-        simpleExoPlayerView.setControllerVisibilityListener(this);
-        simpleExoPlayerView.requestFocus();
+        mApplication = (RandomWebmApplication) getActivity().getApplication();
         mLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
-        tagsRecyclerView.setLayoutManager(mLayoutManager);
-        clearResumePosition();
-        fetchWebmDetails();
+        mTagsRecycler.setLayoutManager(mLayoutManager);
+
+        RandFragmentComponent component = DaggerRandFragmentComponent
+                .builder()
+                .exoPlayerModule(new ExoPlayerModule(getContext(), videoUrl))
+                .build();
+        component.inject(this);
+
+        mPlayerView.setPlayer(player);
+        player.prepare(videoSource);
 
         return v;
     }
 
     @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        int position = getActivity().getResources().getConfiguration().orientation;
-        if (position == Configuration.ORIENTATION_LANDSCAPE) {
-            setLayoutParamsLandscape();
-        }
-    }
-
-        @Override
-    public void onResume() {
-        super.onResume();
-        initializePlayer();
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        releasePlayer();
-    }
-
-    @Override
     public void onStop() {
         super.onStop();
-        releasePlayer();
+        player.release();
     }
 
-    @Override public void onDestroy(){
-        super.onDestroy();
-        disposables.dispose();
-    }
 
-    public void initializePlayer() {
-        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
-        TrackSelection.Factory adaptiveTrackSelectionFactory =
-                new AdaptiveTrackSelection.Factory(bandwidthMeter);
-        TrackSelector trackSelector =
-                new DefaultTrackSelector(adaptiveTrackSelectionFactory);
-
-        player = ExoPlayerFactory.newSimpleInstance(getActivity().getApplicationContext(), trackSelector);
-        simpleExoPlayerView.setPlayer(player);
-        simpleExoPlayerView.setControllerVisibilityListener(playbackControlViewVisibilityListener);
-        player.addListener(this);
-
-        mediaDataSourceFactory = new DefaultDataSourceFactory(getActivity().getApplicationContext(),
-                Util.getUserAgent(getActivity().getApplicationContext(), "Random Webm"), defaultBandwidthMeter);
-        extractorsFactory = new DefaultExtractorsFactory();
-        player.setPlayWhenReady(shouldAutoPlay);
-    }
-
-    private void releasePlayer() {
-        if (player != null) {
-            shouldAutoPlay = player.getPlayWhenReady();
-            updateResumePosition();
-            player.release();
-            player = null;
-        }
-    }
-
-    private void clearResumePosition() {
-        resumeWindow = C.INDEX_UNSET;
-        resumePosition = C.TIME_UNSET;
-    }
-
-    private void updateResumePosition() {
-        resumeWindow = player.getCurrentWindowIndex();
-        resumePosition = player.isCurrentWindowSeekable() ? Math.max(0, player.getCurrentPosition())
-                : C.TIME_UNSET;
-    }
-
-    @Override
-    public void onClick(View view) {
-        if (view == retryButton & player != null) {
-            shouldAutoPlay = player.getPlayWhenReady();
-            player.release();
-            player = null;
-            fetchWebmDetails();
-            initializePlayer();
-        }
-    }
-
-    public void setWebmData(WebmQuery.Data data) {
+    private void setWebmData(WebmQuery.Data data) {
         final WebmQuery.GetWebm getWebm = data.getWebm();
         if (getWebm != null) {
             createdAt.setText(getWebm.createdAt());
-            viewers.setText(String.valueOf(getWebm.views()));
-
-            final List<String> tags = new ArrayList<>();
+            views.setText(String.valueOf(getWebm.views()));
+            final List<String> tagsList = new ArrayList<>();
             for (int i = 0; i < getWebm.tags().size(); ++i) {
-                tags.add(getWebm.tags().get(i).name());
+                tagsList.add(getWebm.tags().get(i).name());
             }
-            tagsAdapter = new TagsAdapter(getContext(), tags);
-            tagsRecyclerView.setAdapter(tagsAdapter);
-            tagsAdapter.SetOnItemClickListener(new TagsAdapter.OnItemClickListener() {
-                @Override
-                public void onItemClick(View view, int position) {
-                    tagName = tagsAdapter.getItem(position).toLowerCase();
-                    FragmentTransaction ft = getFragmentManager().beginTransaction();
-                    Fragment fragment = WebmListFragment.newInstance(order, tagName);
-                    ft.replace(R.id.container, fragment).commit();
-                }
+            mTagsAdapter = new TagsAdapter(getContext(), tagsList);
+            mTagsRecycler.setAdapter(mTagsAdapter);
+            mTagsAdapter.SetOnItemClickListener((view, position) -> {
+                tagName = mTagsAdapter.getItem(position).toLowerCase();
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                Fragment fragment = WebmListFragment.newInstance(order, tagName);
+                ft.replace(R.id.container, fragment).commit();
             });
-
-            // Set video URL
-            videoUrl = getWebm.url();
-            MediaSource extractorMediaSource = new ExtractorMediaSource(Uri.parse(videoUrl),
-                    mediaDataSourceFactory,
-                    extractorsFactory,
-                    null, null);
-            boolean haveResumePosition = resumeWindow != C.INDEX_UNSET;
-            if (haveResumePosition) {
-                player.seekTo(resumeWindow, resumePosition);
-            }
-            player.prepare(extractorMediaSource, !haveResumePosition, false);
         }
     }
 
     private void fetchWebmDetails() {
-        ApolloCall<WebmQuery.Data> webmQuery = application.apolloClient()
+        ApolloCall<WebmQuery.Data> webmQuery = mApplication.apolloClient()
                 .query(new WebmQuery(id));
-        disposables.add(Rx2Apollo.from(webmQuery)
+        mDisposable.add(Rx2Apollo.from(webmQuery)
         .observeOn(AndroidSchedulers.mainThread())
         .subscribeOn(Schedulers.io())
         .subscribeWith(new DisposableObserver<Response<WebmQuery.Data>>() {
@@ -254,7 +153,7 @@ public class RandomFragment extends Fragment implements View.OnClickListener, Ex
 
             @Override
             public void onError(Throwable e) {
-                Log.e(TAG, e.getMessage(), e);
+
             }
 
             @Override
@@ -263,142 +162,4 @@ public class RandomFragment extends Fragment implements View.OnClickListener, Ex
             }
         }));
     }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            setLayoutParamsLandscape();
-        }
-        if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
-            setLayoutParamsPortrait();
-        }
-    }
-
-    private void setLayoutParamsLandscape() {
-        ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
-        getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        SimpleExoPlayerView.LayoutParams params = (SimpleExoPlayerView.LayoutParams) simpleExoPlayerView.getLayoutParams();
-        params.width = ViewGroup.LayoutParams.MATCH_PARENT;
-        params.height = ViewGroup.LayoutParams.MATCH_PARENT;
-        simpleExoPlayerView.setLayoutParams(params);
-    }
-
-    private void setLayoutParamsPortrait() {
-        ((AppCompatActivity) getActivity()).getSupportActionBar().show();
-        getActivity().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        SimpleExoPlayerView.LayoutParams params = (SimpleExoPlayerView.LayoutParams) simpleExoPlayerView.getLayoutParams();
-        params.width = ViewGroup.LayoutParams.MATCH_PARENT;
-        params.height = getResources().getDimensionPixelSize(R.dimen.player_size);
-        simpleExoPlayerView.setLayoutParams(params);
-    }
-
-    /*private void hidePortraitSystemUI() {
-        final View decorView = getActivity().getWindow().getDecorView();
-        decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
-    }*/
-
-    private void showPortraitSystemUI() {
-        final View decorView = getActivity().getWindow().getDecorView();
-        decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-        );
-    }
-
-    private void hideLandscapeSystemUI() {
-        final View decorView = getActivity().getWindow().getDecorView();
-        decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE
-        );
-    }
-
-    private void showLandscapeSystemUI() {
-        final View decorView = getActivity().getWindow().getDecorView();
-        decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-        );
-    }
-
-    private PlaybackControlView.VisibilityListener playbackControlViewVisibilityListener = new PlaybackControlView.VisibilityListener() {
-        @Override
-        public void onVisibilityChange(int visibility) {
-            int orientation = getContext().getResources().getConfiguration().orientation;
-            switch (orientation) {
-                case Configuration.ORIENTATION_PORTRAIT:
-                    if (visibility == View.GONE) {
-                        showPortraitSystemUI();
-                    }
-                    break;
-                case Configuration.ORIENTATION_LANDSCAPE:
-                    if (visibility == View.GONE) {
-                        hideLandscapeSystemUI();
-                    } else {
-                        showLandscapeSystemUI();
-                    }
-                    break;
-                default:
-                    break;
-            }
-        }
-    };
-
-    @Override
-    public void onTimelineChanged(Timeline timeline, Object manifest) {
-
-    }
-
-    @Override
-    public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
-
-    }
-
-    @Override
-    public void onLoadingChanged(boolean isLoading) {
-
-    }
-
-    @Override
-    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
-        if (playbackState == player.STATE_BUFFERING) {
-            progressBar.setVisibility(View.VISIBLE);
-        } else {
-            progressBar.setVisibility(View.INVISIBLE);
-        }
-    }
-
-    @Override
-    public void onRepeatModeChanged(int repeatMode) {
-
-    }
-
-    @Override
-    public void onPlayerError(ExoPlaybackException error) {
-
-    }
-
-    @Override
-    public void onPositionDiscontinuity() {
-
-    }
-
-    @Override
-    public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
-
-    }
-
-    @Override
-    public void onVisibilityChange(int visibility) {
-
-    }
-
 }
