859074257a5ab017263e1730b9331be39351c9f2
Alexey Kochergin <alkocherg@gmail.com>
2018-03-20 22:53:29 +0700

    Create VideoPlayerModule for ExoPlayer

--- app/src/main/java/com/example/aleksejkocergin/randomwebm/module/ExoPlayerModule.java
+++ app/src/main/java/com/example/aleksejkocergin/randomwebm/module/ExoPlayerModule.java
@@ -1,16 +1,10 @@
 package com.example.aleksejkocergin.randomwebm.module;
 
-
 import android.content.Context;
-import android.net.Uri;
 
-import com.google.android.exoplayer2.DefaultLoadControl;
+import com.example.aleksejkocergin.randomwebm.components.PlayerScope;
 import com.google.android.exoplayer2.ExoPlayerFactory;
-import com.google.android.exoplayer2.LoadControl;
 import com.google.android.exoplayer2.SimpleExoPlayer;
-import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
-import com.google.android.exoplayer2.source.ExtractorMediaSource;
-import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.trackselection.TrackSelection;
@@ -18,82 +12,54 @@ import com.google.android.exoplayer2.trackselection.TrackSelector;
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
-import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
-import com.google.android.exoplayer2.upstream.HttpDataSource;
 import com.google.android.exoplayer2.util.Util;
-
-import android.os.Handler;
 
 import dagger.Module;
 import dagger.Provides;
 
-
 @Module
 public class ExoPlayerModule {
-
     private final Context context;
-    private String videoUri;
-    public ExoPlayerModule(Context context, String  playerUri) {
+
+    public ExoPlayerModule(Context context) {
         this.context = context;
-        this.videoUri = playerUri;
     }
 
     @Provides
+    @PlayerScope
     public Context context() {
         return context;
     }
 
     @Provides
-    public Handler provideVideoHandler() {
-        return new Handler();
-    }
-
-    @Provides
-    public DefaultBandwidthMeter providesDefaultBandwidthMeter() {
+    @PlayerScope
+    public DefaultBandwidthMeter providesBandwidthMeter() {
         return new DefaultBandwidthMeter();
     }
 
     @Provides
-    public TrackSelection.Factory providesVideoTrackSelection(DefaultBandwidthMeter bandwidthMeter) {
+    @PlayerScope
+    public TrackSelection.Factory providesTrackSelectionFactory(DefaultBandwidthMeter bandwidthMeter) {
         return new AdaptiveTrackSelection.Factory(bandwidthMeter);
     }
 
     @Provides
-    public TrackSelector providesTracSelector(TrackSelection.Factory videoTrackSelectionFactory) {
+    @PlayerScope
+    public TrackSelector providesTrackSelectorFactory(TrackSelection.Factory videoTrackSelectionFactory) {
         return new DefaultTrackSelector(videoTrackSelectionFactory);
     }
 
     @Provides
-    public LoadControl providesLoadControl() {
-        return new DefaultLoadControl();
+    @PlayerScope
+    public SimpleExoPlayer providesExoPlayer(Context context, TrackSelector trackSelector) {
+        return ExoPlayerFactory.newSimpleInstance(context, trackSelector);
     }
 
     @Provides
-    public SimpleExoPlayer provideExoPlayer(
-            Context context, TrackSelector trackSelector, LoadControl loadControl) {
-        return ExoPlayerFactory.newSimpleInstance(context, trackSelector, loadControl);
-    }
-
-    @Provides
-    public String providesUserAgent() {
-        return Util.getUserAgent(context, "RandomWebm");
-    }
-
-    @Provides
-    public HttpDataSource.Factory providesHttpDataSource(
-            String userAgent, DefaultBandwidthMeter bandwidthMeter) {
-        return new DefaultHttpDataSourceFactory(userAgent, bandwidthMeter);
-    }
-
-    @Provides
-    public DataSource.Factory providesDataSource(
-            Context context, DefaultBandwidthMeter bandwidthMeter, HttpDataSource.Factory httpDataSource) {
-        return new DefaultDataSourceFactory(context, bandwidthMeter, httpDataSource);
-    }
-
-    @Provides
-    public MediaSource providesMediaSource(DataSource.Factory dataSourceFactory, Handler mainHandler) {
-        Uri uri = Uri.parse(videoUri);
-        return new ExtractorMediaSource(uri, dataSourceFactory, new DefaultExtractorsFactory(), mainHandler, null);
+    @PlayerScope
+    public DataSource.Factory providesDataSourceFactory(Context context) {
+        return new DefaultDataSourceFactory(
+                context, Util.getUserAgent(context, "RandomWebm"),
+                new DefaultBandwidthMeter());
     }
 }
