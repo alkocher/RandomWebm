da07bbb58c9ef4ac8daee9989b60c5e947b08f65
Alexey Kochergin <alkocherg@gmail.com>
2018-03-26 12:24:25 +0700

    Create interface for like/dislike

---
+++ app/src/main/java/com/example/aleksejkocergin/randomwebm/interfaces/ToggleVotes.java
@@ -0,0 +1,9 @@
+package com.example.aleksejkocergin.randomwebm.interfaces;
+
+import com.example.aleksejkocergin.myapplication.WebmQuery;
+
+public interface ToggleVotes {
+
+    void plusWebmLike();
+    void plusWebmDislike();
+}
