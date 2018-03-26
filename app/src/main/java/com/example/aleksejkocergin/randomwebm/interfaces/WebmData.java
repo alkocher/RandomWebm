8ceb69db733e3432bad1d7c46677547914d673d5
Alexey Kochergin <alkocherg@gmail.com>
2018-03-26 12:17:08 +0700

    Create interface

---
+++ app/src/main/java/com/example/aleksejkocergin/randomwebm/interfaces/WebmData.java
@@ -0,0 +1,9 @@
+package com.example.aleksejkocergin.randomwebm.interfaces;
+
+
+import com.example.aleksejkocergin.myapplication.WebmQuery;
+
+public interface WebmData {
+
+    void setWebmData(WebmQuery.Data data);
+}
