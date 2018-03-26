9f3bb538add6c5cff60404d8415a7d5a0a5721e6
Alexey Kochergin <alkocherg@gmail.com>
2018-03-26 12:12:15 +0700

    Create WebmDetailsFetcher.class

---
+++ app/src/main/java/com/example/aleksejkocergin/randomwebm/util/WebmDetailsFetcher.java
@@ -0,0 +1,50 @@
+package com.example.aleksejkocergin.randomwebm.util;
+
+import com.apollographql.apollo.ApolloCall;
+import com.apollographql.apollo.api.Response;
+import com.apollographql.apollo.rx2.Rx2Apollo;
+import com.example.aleksejkocergin.myapplication.WebmQuery;
+import com.example.aleksejkocergin.randomwebm.interfaces.WebmData;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
+
+
+public class WebmDetailsFetcher {
+
+    private CompositeDisposable disposable = new CompositeDisposable();
+    private WebmData webmData;
+
+    private String webmId = "";
+
+    public WebmDetailsFetcher(WebmData webmData) {
+        this.webmData = webmData;
+    }
+
+    public WebmDetailsFetcher(WebmData webmData, String webmId) {
+        this.webmData = webmData;
+        this.webmId = webmId;
+    }
+
+    public void fetchWebmDetails() {
+        ApolloCall<WebmQuery.Data> webmQuery = WebmApolloClient.getWebmApolloClient()
+                .query(new WebmQuery(webmId));
+        disposable.add(Rx2Apollo.from(webmQuery)
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribeOn(Schedulers.io())
+        .subscribeWith(new DisposableObserver<Response<WebmQuery.Data>>() {
+            @Override
+            public void onNext(Response<WebmQuery.Data> dataResponse) {
+                webmData.setWebmData(dataResponse.data());
+            }
+
+            @Override
+            public void onError(Throwable e) {}
+
+            @Override
+            public void onComplete() {}
+        }));
+    }
+}
