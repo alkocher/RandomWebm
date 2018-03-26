3b3e92d139cd77881cb2d7c31c48b4e39c015e2a
Alexey Kochergin <alkocherg@gmail.com>
2018-03-26 12:25:01 +0700

    Delete import

--- app/src/main/java/com/example/aleksejkocergin/randomwebm/interfaces/ToggleVotes.java
+++ app/src/main/java/com/example/aleksejkocergin/randomwebm/interfaces/ToggleVotes.java
@@ -1,7 +1,5 @@
 package com.example.aleksejkocergin.randomwebm.interfaces;
 
-import com.example.aleksejkocergin.myapplication.WebmQuery;
-
 public interface ToggleVotes {
 
     void plusWebmLike();
