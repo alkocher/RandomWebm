0b727db93d3550ef670ec25f08cbff7a6146c585
Alexey Kochergin <alkocherg@gmail.com>
2018-03-26 12:26:24 +0700

    Create ApolloMutationCall for like/dislike

---
+++ app/src/main/java/com/example/aleksejkocergin/randomwebm/util/ToggleVotesUtil.java
@@ -0,0 +1,73 @@
+package com.example.aleksejkocergin.randomwebm.util;
+
+import com.apollographql.apollo.ApolloMutationCall;
+import com.apollographql.apollo.api.Response;
+import com.apollographql.apollo.rx2.Rx2Apollo;
+import com.example.aleksejkocergin.myapplication.ToggleDislikeMutation;
+import com.example.aleksejkocergin.myapplication.ToggleLikeMutation;
+import com.example.aleksejkocergin.randomwebm.interfaces.ToggleVotes;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
+
+public class ToggleVotesUtil {
+
+    private CompositeDisposable mDisposable = new CompositeDisposable();
+    private ToggleVotes toggleVotes;
+
+    public ToggleVotesUtil(ToggleVotes toggleVotes) {
+        this.toggleVotes = toggleVotes;
+    }
+
+    public void toggleLike(String webmId, boolean hasLike, boolean hasDislike) {
+        ApolloMutationCall<ToggleLikeMutation.Data> likeMutationCall =
+                WebmApolloClient.getWebmApolloClient()
+                    .mutate(new ToggleLikeMutation(webmId, hasLike, hasDislike));
+        mDisposable.add(Rx2Apollo.from(likeMutationCall)
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribeOn(Schedulers.io())
+        .subscribeWith(new DisposableObserver<Response<ToggleLikeMutation.Data>>() {
+            @Override
+            public void onNext(Response<ToggleLikeMutation.Data> dataResponse) {
+                dataResponse.data();
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+                toggleVotes.plusWebmLike();
+            }
+        }));
+    }
+
+    public void toggleDislike(String webmId, boolean hasLike, boolean hasDislike) {
+        ApolloMutationCall<ToggleDislikeMutation.Data> dislikeMutationCall =
+                WebmApolloClient.getWebmApolloClient()
+                        .mutate(new ToggleDislikeMutation(webmId, hasLike, hasDislike));
+        mDisposable.add(Rx2Apollo.from(dislikeMutationCall)
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribeOn(Schedulers.io())
+        .subscribeWith(new DisposableObserver<Response<ToggleDislikeMutation.Data>>() {
+            @Override
+            public void onNext(Response<ToggleDislikeMutation.Data> dataResponse) {
+                dataResponse.data();
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+                toggleVotes.plusWebmDislike();
+            }
+        }));
+    }
+}
