06bd2b21ce35daf42c9def3a0f5b6155f536e882
Alexey Kochergin <alkocherg@gmail.com>
2018-06-05 21:25:49 +0700

    Add new methods on interface

--- app/src/main/java/com/example/aleksejkocergin/randomwebm/interfaces/WebmData.java
+++ app/src/main/java/com/example/aleksejkocergin/randomwebm/interfaces/WebmData.java
@@ -6,4 +6,6 @@ import com.example.aleksejkocergin.myapplication.WebmQuery;
 public interface WebmData {
 
     void render(WebmQuery.Data data);
+    void showSuccessSnackbar();
+    void showErrorSnackbar();
 }
